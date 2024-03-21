3c3e33609403388f30ae0b1d1e4fe5d8c1cfc5d4
Ty Jac <muzuuu@protonmail.com>
2024-03-21 21:36:28 +0800

    chore: add user validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,26 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable,
     :omniauthable, omniauth_providers: [:google_oauth2]
 
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :last_name, length: { minimum: 2, maximum: 30 }, allow_blank: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validate :password_complexity
+
+  def password_complexity
+    return unless password.present?
+
+    errors.add(:password, 'cannot be longer than 128 characters') unless password.length <= 128
+    unless password.match?(/[a-zA-Z]/)
+      errors.add(:password, 'must contain at least one letter')
+    end
+    unless password.match?(/\d/)
+      errors.add(:password, 'must contain at least one number')
+    end
+    unless password.match?(/[!@#$%^&*]/)
+      errors.add(:password, 'must contain at least one special character')
+    end
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:user_valid) { users(:valid_user) }
+    let(:user_missing_first_name) { users(:missing_first_name_user) }
+    let(:user_short_first_name) { users(:short_first_name_user) }
+    let(:user_long_first_name) { users(:long_first_name_user) }
+    let(:user_missing_last_name) { users(:missing_last_name_user) }
+    let(:user_short_last_name) { users(:short_last_name_user) }
+    let(:user_long_last_name) { users(:long_last_name_user) }
+    let(:user_invalid_email) { users(:invalid_email_user) }
+
+    context 'when the user is valid' do
+      it 'is valid' do
+        expect(user_valid).to be_valid
+      end
+    end
+
+    context 'when first name is invalid' do 
+      it 'is invalid without a first name' do
+        expect(user_missing_first_name).to be_invalid
+      end
+      
+      it 'is invalid with a short first name' do
+        expect(user_short_first_name).to be_invalid
+      end
+
+      it 'is invalid with a long first name' do
+        expect(user_long_first_name).to be_invalid
+      end
+    end
+
+      context 'when last name is invalid' do
+      it 'is invalid with a short last name' do
+        expect(user_short_last_name).to be_invalid
+      end
+
+      it 'is invalid with a long last name' do
+        expect(user_long_last_name).to be_invalid
+      end
+    end
+
+    context 'when the email is invalid' do
+      it 'is not a valid email' do
+        expect(user_invalid_email).to be_invalid
+      end
+    end
+  end
 end
