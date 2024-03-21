4372b9eac8b2c88ba06025a04bc6ca80294a3df9
Ty Jac <muzuuu@protonmail.com>
2024-03-22 00:01:35 +0800

    chore: create avatar component

---
+++ app/helpers/components/avatar_helper.rb
@@ -0,0 +1,8 @@
+module Components::AvatarHelper
+  def render_avatar(src: nil, data: {}, **options, &block)
+    avatar_classes = "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full"
+    avatar_classes << " #{options[:class]}"
+    avatar_classes = tw(avatar_classes)
+    render "components/ui/avatar", avatar_classes:, src:, data:, **options
+  end
+end
