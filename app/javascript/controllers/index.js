9fea5cdbb5f218d15815120ddce78cb8d7d16086
Ty Jac <muzuuu@protonmail.com>
2024-03-21 22:14:46 +0800

    feat: add notice and alert toasts

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import Ui__ToastController from "./ui/toast_controller"
+application.register("ui--toast", Ui__ToastController)
