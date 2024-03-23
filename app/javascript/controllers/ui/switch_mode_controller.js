9b3b2431e6d0c9e9c7dc3d8b89b474d7ddcbd1f5
Ty Jac <muzuuu@protonmail.com>
2024-03-23 11:39:37 +0800

    fix: toggle switch on dark mode mount

--- app/javascript/controllers/ui/switch_mode_controller.js
+++ app/javascript/controllers/ui/switch_mode_controller.js
@@ -2,23 +2,37 @@ import { Controller } from "@hotwired/stimulus";
 import UIToggleController from "./switch_controller.js";
 
 export default class UIToggleModeController extends UIToggleController {
+  connect() {
+    this.root = window.document.documentElement;
+    this.storageKey = "ui-theme";
+    this.systemTheme = null;
+    if(!localStorage.getItem(this.storageKey)) {
+      this.systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches? "light" : this.setDark();
+    }
+  }
+
+  setDark() {
+    this.systemTheme = "dark";
+    console.log("set dark: ", this.systemTheme);
+    this.setTheme(); 
+    this.toggle();
+  }
+
   toggleMode() {
     if (this.element.dataset.state == "checked") {
-      this.systemTheme = "dark";
-    } else {
       this.systemTheme = "light";
+    } else {
+      this.systemTheme = "dark";
     };
     
     this.setTheme();
     this.toggle();
+    console.log(this.systemTheme)
   }
 
   setTheme() {
-    const root = window.document.documentElement;
-    const storageKey = "ui-theme";
-
-    root.classList.remove("light", "dark");
-    root.classList.add(this.systemTheme);
-    localStorage.setItem(storageKey, this.systemTheme);
+    this.root.classList.remove("light", "dark");
+    this.root.classList.add(this.systemTheme);
+    localStorage.setItem(this.storageKey, this.systemTheme);
   }
 }
