c7155c62a5745761174d67e14c0ff057c199f0fc
Ty Jac <muzuuu@protonmail.com>
2024-03-24 13:00:21 +0800

    feat: add config for google smtp mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,9 +73,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
+  # Send emails via google smtp
+  config.action_mailer.default_url_options = {host: Rails.application.credentials.dig(:host) , protocol: 'https'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials.dig(:google_smtp, :email),
+    password:             Rails.application.credentials.dig(:google_smtp, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
