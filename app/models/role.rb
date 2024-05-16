ca108534f3230b6ae9da3d0acc6e9ce5821b5fe9
maeci <cimafrancamae@gmail.com>
2024-05-16 18:50:22 +0800

    refactor: role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,14 +1,25 @@
 # frozen_string_literal: true
 
 class Role < ApplicationRecord
-  store :permissions, accessors: %i[
-    create_services
-    read_services
-    update_services
-    delete_services
-    create_appointments
-    read_appointments
-    update_appointments
-    delete_appointments
-  ]
+  # Define accessors for the JSON attributes
+  store_accessor :permissions, :create_services, :read_services, :update_services, :delete_services
+  store_accessor :permissions, :create_appointments, :read_appointments, :update_appointments, :delete_appointments
+
+  # Ensure default values are set
+  after_initialize :set_default_permissions, if: :new_record?
+
+  private
+
+  def set_default_permissions
+    self.permissions ||= {
+      create_services: false,
+      read_services: false,
+      update_services: false,
+      delete_services: false,
+      create_appointments: false,
+      read_appointments: false,
+      update_appointments: false,
+      delete_appointments: false
+    }
+  end
 end
