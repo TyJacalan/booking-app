bd0a6ae5734451a4c5b473a17fd2cceda9bf5f81
p3rc1us <kervymaniego@gmail.com>
2024-05-16 15:37:41 +0800

    removed def address_format and validate_address_is_real_place, added error handling in set_address

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,6 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validate :password_complexity
-  validate :address_format
-  validate :validate_address_is_real_place
 
   after_validation :geocode, if: -> { address.present? && address_changed? }
 
@@ -43,26 +41,7 @@ class User < ApplicationRecord
 
   def set_address
     self.address = "#{city}, #{country}" if city.present? && country.present?
-  end
-
-  def address_format
-    if address.blank?
-      errors.add(:address, "can't be blank")
-    else
-      parts = address.split(',')
-      if parts.length != 2 || parts.any?(&:blank?)
-        errors.add(:address, 'must include a city and country separated by a comma')
-      end
-    end
-  end
-
-  def validate_address_is_real_place
-    return unless address.present?
-
-    geocode = Geocoder.search(address).first
-    errors.add(:address, 'must correspond to a real place') unless geocode && geocode.coordinates.present?
-  rescue StandardError => e
-    errors.add(:address, 'could not be validated')
+    errors.add(:city, 'must correspond to a real city') unless Geocoder.search(address).first
   end
 
   def set_fullname
