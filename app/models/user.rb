ee19beab578d52a5ad55526251369a251ab9c1ff
p3rc1us <kervymaniego@gmail.com>
2024-05-16 12:05:29 +0800

    Add address validation to ensure user-provided addresses correspond to real places
    - Implement validation using geocoding to verify address accuracy
    - Handle cases where addresses cannot be geocoded or do not correspond to real places

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         #:omniauthable, omniauth_providers: [:google_oauth2]
   geocoded_by :address
-
 
   before_validation :set_address
 
   validates :first_name, :last_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validate :password_complexity
+  validate :address_format
+  validate :validate_address_is_real_place
 
   after_validation :geocode, if: -> { address.present? && address_changed? }
 
@@ -43,6 +43,26 @@ class User < ApplicationRecord
 
   def set_address
     self.address = "#{city}, #{country}" if city.present? && country.present?
+  end
+
+  def address_format
+    if address.blank?
+      errors.add(:address, "can't be blank")
+    else
+      parts = address.split(',')
+      if parts.length != 2 || parts.any?(&:blank?)
+        errors.add(:address, 'must include a city and country separated by a comma')
+      end
+    end
+  end
+
+  def validate_address_is_real_place
+    return unless address.present?
+
+    geocode = Geocoder.search(address).first
+    errors.add(:address, 'must correspond to a real place') unless geocode && geocode.coordinates.present?
+  rescue StandardError => e
+    errors.add(:address, 'could not be validated')
   end
 
   def set_fullname
