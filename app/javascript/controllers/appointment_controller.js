7959dfc5b819503795d5fdf2a500ec5f1776ac11
Ty Jac <muzuuu@protonmail.com>
2024-05-18 15:21:16 +0800

    feat: add create appointment functionality

--- app/javascript/controllers/appointment_controller.js
+++ app/javascript/controllers/appointment_controller.js
@@ -1,34 +1,87 @@
 import { Controller } from '@hotwired/stimulus'
 import flatpickr from 'flatpickr'
 
-// Connects to data-controller="appointment"
 export default class extends Controller {
-  static targets = ['startDate', 'endDate']
+  static targets = ['dateInput',
+                    'duration',
+                    'durationInput',
+                    'endDate', 
+                    'fee',
+                    'price',
+                    'serviceFee',
+                    'startDate',
+                    'totalFee' ]
 
   connect() {
-    flatpickr(this.startDateTargets, {
+    flatpickr(this.dateInputTargets, {
+      mode: 'range',
       minDate: new Date().fp_incr(1),
-      onChange: (selectedDates, dateStr, instance) => {
-        console.log(selectedDates)
-        this.triggerEndDatePicker(selectedDates);
+      onChange: (selectedDates) => {
+        this.handleDateChange(selectedDates);
+        this.handleDurationChange();
       },
     });
 
+  }
+
+  handleDateChange(selectedDates) {
+    const startDate = selectedDates[0];
+    const endDate = selectedDates[1];
+
+    this.startDateTarget.value = selectedDates[0];
+    this.endDateTarget.value = selectedDates[1];
+
+    const totalHours = this.calculateTotalAvailableHours(startDate, endDate);
+    this.updateDurationOptions(totalHours);
+
+    
     this.updateDetails();
   }
 
-  triggerEndDatePicker(startDate){
-    flatpickr(this.endDateTarget, {
-      minDate: new Date(startDate),
-      onChange: (selectedDates, dateStr, instance) => {
-      },
+  handleDurationChange(){
+    this.durationInputTarget.addEventListener('change', () => {
+      this.updateDetails();
     });
-
-    this.endDateTarget.click();
   }
 
-  updateDetails(){
-    // calculate total price 
-    // add service fee
+  calculateTotalAvailableHours(startDate, endDate) {
+    const minimumAvailableHours = 24;
+    const dateDifference = endDate  - startDate;
+
+    const millisecondsInAnHour = 1000 * 60 * 60;
+    return Math.ceil( dateDifference / millisecondsInAnHour ) || minimumAvailableHours;
+  }
+
+  updateDurationOptions(totalHours) {
+    this.durationInputTarget.innerHTML = '';
+    for (let i = 1; i <= totalHours; i++) {
+      const option = document.createElement('option');
+      option.value = i;
+      option.textContent = i;
+      this.durationInputTarget.appendChild(option);
+      
+    }
+  }
+
+  updateDetails() {
+    const duration = parseInt(this.durationInputTarget.value);
+    const fee = parseInt(this.feeTarget.textContent);
+    const price = duration * fee;
+    const serviceFee = price * 0.025;
+    const totalFee = price + serviceFee;
+
+    this.durationTarget.textContent = duration;
+    this.priceTarget.textContent = this.formatCurrency(price);
+    this.serviceFeeTarget.textContent = this.formatCurrency(serviceFee);
+    this.totalFeeTarget.textContent = this.formatCurrency(totalFee);
+  }
+
+  formatCurrency(value) {
+    const formatter = new Intl.NumberFormat('en-PH', {
+      style: 'currency',
+      currency: 'PHP',
+    });
+
+    return formatter.format(value);
   }
 }
