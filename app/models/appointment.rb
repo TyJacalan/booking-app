0b643a192b7ba867bf5d62d9bdc1f830b903a4f3
Ty Jac <muzuuu@protonmail.com>
2024-05-20 17:14:10 +0800

    chore: add delete appointment functionality

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -8,6 +8,7 @@ class Appointment < ApplicationRecord
   enum status: { pending: 0, accepted: 1, denied: 2, expired: 3, paid: 4, blocked: 5 }
 
   before_save :set_price
+  before_destroy :validate_deletion
 
   private
 
@@ -19,4 +20,13 @@ class Appointment < ApplicationRecord
     fee = price + service_fee
   end
 
+  def validate_deletion
+    if status != 'pending'
+      errors.add(:base, 'Appointment cannot be deleted unless it is pending')
+      throw(:abort)
+    elsif start < 3.days.from_now
+      errors.add(:base, 'Appointment cannot be deleted within three (3) days of the start date')
+      throw(:abort)
+    end
+  end
 end
