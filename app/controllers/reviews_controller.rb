a5bb297df564c154c73e4b7abfa86adb204e6233
maeci <cimafrancamae@gmail.com>
2024-05-21 03:54:24 +0800

    feat: display reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,22 @@
 class ReviewsController < ApplicationController
-  def new; end
+  before_action :set_service, only: %i[index]
+  before_action :set_reviews, only: %i[index]
+
+  rescue_from Pundit::NotAuthorizedError do |e|
+    Rails.logger.error("Authorization failed: #{e.message}")
+    redirect_to root_path, alert: 'Unauthorized access: Reviews'
+  end
+
+  def index; end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def set_reviews
+    @reviews = policy_scope(Review).where(service_id: @service.id)
+    authorize @reviews
+  end
 end
