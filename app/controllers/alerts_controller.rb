d969405156f53ba6fb0f14b48c995a5d385b4277
Ty Jac <muzuuu@protonmail.com>
2024-05-23 17:51:20 +0800

    feat: add link to notifications path from alerts

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,6 +1,6 @@
 class AlertsController < ApplicationController
   def index
-    @notifications = current_user.notifications.order(:created_at).limit(5)
+    @notifications = current_user.notifications.order(created_at: :desc).limit(5)
     @count_unread = current_user.notifications.where(read: false).count
     authorize @notifications
   end
