e91cbafa4d9b36e6079fd15356a49c04498391ff
acostayuukichi <acostayuukichi@gmail.com>
2024-05-26 15:46:40 +0800

    deletes view file for pr

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,11 @@
 class ReviewsController < ApplicationController
-  before_action :set_service, only: %i[index new create]
   before_action :set_review, only: %i[show edit update destroy]
-  before_action :set_reviews, only: %i[index]
+  before_action :set_appointment, only: %i[new create]
 
   # GET /services/:service_id/reviews
   def index
+    @reviews = Review.where(service_id: params[:service_id])
+
     respond_to do |format|
       format.js { render partial: 'reviews/reviews_list', locals: { reviews: @reviews } }
       format.html # renders index.html.erb by default
@@ -14,30 +15,29 @@ class ReviewsController < ApplicationController
 
   # GET /reviews/:id
   def show
-    @comments = @review.comments
-
     respond_to do |format|
       format.js { render partial: 'reviews/review_with_comments', locals: { review: @review, comments: @comments } }
       format.html # renders show.html.erb by default
     end
   end
 
-  # GET /services/:service_id/appointment/:appointment_id/reviews/new
+  # GET appointment/:appointment_id/reviews/new
   def new
-    @review = @service.reviews.new
+    @review = @appointment.reviews.new
   end
 
   # GET /reviews/:id/edit
   def edit
   end
 
-  # POST /services/:service_id/appointment/:appointment_id/reviews
+  # POST appointment/:appointment_id/reviews
   # edit
   def create
     ActiveRecord::Base.transaction do
       @review = @appoitment.build_reviews(review_params)
-      @review.appointment_id = params[:appointment_id]
+      @review.service_id = @appointment.service.id
       @review.client_id = current_user.id
+      @review.freelancer_id = @appointment.freelancer.id
   
       if @review.save!
         redirect_to service_review_path(@service, @review), notice: 'Review was successfully created.'
@@ -69,17 +69,12 @@ class ReviewsController < ApplicationController
 
   private
 
-  def set_service
-    @service = Service.find(params[:service_id])
+  def set_appointment
+    @appointment = Appointment.find(params[:appointment_id])
   end
 
   def set_review
-    @review = Review.find(params[:id])
-  end
-
-  def set_reviews
-    @reviews = policy_scope(Review).where(service_id: @service.id)
-    authorize @reviews
+    @review = Review.find(params[:review_id])
   end
 
   def review_params
