1d7a932fb01acee2b491f3f01f1b099f5cb87403
acostayuukichi <acostayuukichi@gmail.com>
2024-05-26 16:02:36 +0800

    fixs controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,89 @@
 class CommentsController < ApplicationController
-    before_action :set_comment
-    before_action :set_review
-    
-    def new
+  before_action :set_review, onyl: [:new, :create]
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  # GET /reviews/:review_id/comments
+  def index
+    @comments = @review.comments
+    respond_to do |format|
+      format.html # renders index.html.erb
+      format.json { render json: @comments }
     end
+  end
 
-    def create
-        @comment = @review.comments.build(comment_params)
-    
-        if @comment.save
-          CommentsChannel.broadcast_to(@review, @comment)
-          render partial: 'comments/comment', locals: { comment: @comment }
-        else
-          # Handle error
-        end
+  # GET /comments/:id
+  def show
+    respond_to do |format|
+      format.html # renders show.html.erb
+      format.json { render json: @comment }
     end
+  end
 
-    def destroy 
+  # GET /reviews/:review_id/comments/new
+  def new
+    @comment = @review.comments.new
+  end
+
+  # POST /reviews/:review_id/comments
+  def create
+    @comment = @review.comments.build(comment_params)
+    @comment.user = current_user # assuming a user is creating the comment
+
+    if @comment.save
+      CommentsChannel.broadcast_to(@review, @comment)
+      respond_to do |format|
+        format.html { redirect_to review_path(@review), notice: 'Comment was successfully created.' }
+        format.json { render json: @comment, status: :created }
+        format.js { render partial: 'comments/comment', locals: { comment: @comment } }
+      end
+    else
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.js { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    private
+  # GET /comments/:id/edit
+  def edit
+  end
 
-    def set_comment
+  # PATCH/PUT /comments/:id
+  def update
+    if @comment.update(comment_params)
+      respond_to do |format|
+        format.html { redirect_to review_path(@review), notice: 'Comment was successfully updated.' }
+        format.json { render json: @comment, status: :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def set_review
-        @review = Review.find(params[:review_id])
+  # DELETE /comments/:id
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to review_path(@review), notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
     end
+  end
 
-    def comment_params
-        params.require(:comment).permit(:subject)
-    end   
+  private
+
+  def set_comment
+    @comment = @review.comments.find(params[:id])
+  end
+
+  def set_review
+    @review = Review.find(params[:review_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:subject, :content)
+  end
 end
-
