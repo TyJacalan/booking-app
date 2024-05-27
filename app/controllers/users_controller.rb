4b7dddba35b37e46138b533e4cec23de2bfca3e3
maeci <cimafrancamae@gmail.com>
2024-05-27 09:48:48 +0800

    refactor: show  freelancer profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,44 +9,33 @@ class UsersController < ApplicationController
 
   def index; end
 
-  def show
-    authorize User
-  end
+  def show; end
 
   def reviews; end
 
   private
 
   def set_user
-    if current_user
-      @user = User.find_by(id: current_user.id)
-      return @user if @user
-    end
-    redirect_to root_path
+    @user = User.find(params[:id])
+    authorize User, :show?
   end
 
   def set_services
-    @services = current_user.services if current_user&.role&.name == 'freelancer'
+    @all_services = @user.services
+    @services = @user.services.page(params[:page]).per(5)
   end
 
   def set_reviews
-    @reviews = if current_user&.role&.name == 'freelancer'
-                 Review.where(freelancer_id: current_user.id).page(params[:page]).per(5)
-               else
-                 Review.where(client_id: current_user.id).page(params[:page]).per(5)
-               end
+    @all_reviews = Review.where(freelancer_id: @user.id)
+    @reviews = Review.where(freelancer_id: @user.id).page(params[:page]).per(5)
     authorize User, :show?
   end
 
   def set_appointments
-    @appointments = if current_user&.role&.name == 'freelancer'
-                      Appointment.where(freelancer_id: current_user.id).page(params[:page]).per(5)
-                    else
-                      Appointment.where(client_id: current_user.id).page(params[:page]).per(5)
-                    end
+    @appointments = Appointment.where(freelancer_id: @user.id).page(params[:page]).per(5)
   end
 
   def set_accepted_appointments
-    @accepted_appointments = Appointment.where(freelancer_id: current_user.id, status: :accepted)
+    @accepted_appointments = Appointment.where(freelancer_id: @user.id, status: :accepted)
   end
 end
