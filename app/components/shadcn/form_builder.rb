2ced2b44a1e871001b02297e572ce1497e4e338c
Ty Jac <muzuuu@protonmail.com>
2024-05-27 21:39:36 +0800

    fix: generate correct name and id when @object is not passed

--- app/components/shadcn/form_builder.rb
+++ app/components/shadcn/form_builder.rb
@@ -13,12 +13,14 @@ class Shadcn::FormBuilder < ActionView::Helpers::FormBuilder
 
   def text_field(method, options = {})
     error_class = @object&.errors&.[](method)&.any? ? 'error' : ''
+    name = object_name ? "#{object_name}[#{method}]" : method.to_s
+    id = object_name ? "#{object_name}_#{method}" : method.to_s
     options[:class] = @template.tw("#{options[:class]} #{error_class}")
     value = @object&.send(method) || ''
 
     @template.render_input(
-      name: "#{object_name}[#{method}]",
-      id: "#{object_name}_#{method}",
+      name: name,
+      id: id,
       value: value,
       type: 'text', **options
     )
@@ -26,33 +28,45 @@ class Shadcn::FormBuilder < ActionView::Helpers::FormBuilder
 
   def number_field(method, options = {})
     error_class = @object&.errors&.[](method)&.any? ? 'error' : ''
+    name = object_name ? "#{object_name}[#{method}]" : method.to_s
+    id = object_name ? "#{object_name}_#{method}" : method.to_s
     options[:class] = @template.tw("#{options[:class]} #{error_class}")
+    value = @object&.send(method) || ''
+
     @template.render_input(
-      name: "#{object_name}[#{method}]",
-      id: "#{object_name}_#{method}",
-      value: @object.send(method),
+      name: name,
+      id: id,
+      value: value,
       type: 'number', **options
     )
   end
 
   def password_field(method, options = {})
     error_class = @object&.errors&.[](method)&.any? ? 'error' : ''
+    name = object_name ? "#{object_name}[#{method}]" : method.to_s
+    id = object_name ? "#{object_name}_#{method}" : method.to_s
     options[:class] = @template.tw("#{options[:class]} #{error_class}")
+    value = @object&.send(method) || ''
+
     @template.render_input(
-      name: "#{object_name}[#{method}]",
-      id: "#{object_name}_#{method}",
-      value: @object.send(method),
+      name: name,
+      id: id,
+      value: value,
       type: 'password', **options
     )
   end
 
   def email_field(method, options = {})
     error_class = @object&.errors&.[](method)&.any? ? 'error' : ''
+    name = object_name ? "#{object_name}[#{method}]" : method.to_s
+    id = object_name ? "#{object_name}_#{method}" : method.to_s
     options[:class] = @template.tw("#{options[:class]} #{error_class}")
+    value = @object&.send(method) || ''
+
     @template.render_input(
-      name: "#{object_name}[#{method}]",
-      id: "#{object_name}_#{method}",
-      value: @object.send(method),
+      name: name,
+      id: id,
+      value: value,
       type: 'email', **options
     )
   end
@@ -60,11 +74,14 @@ class Shadcn::FormBuilder < ActionView::Helpers::FormBuilder
   def text_area(method, options = {}, &block)
     error_class = @object&.errors&.[](method)&.any? ? 'error' : ''
     options[:class] = @template.tw("#{options[:class]} #{error_class}")
+    name = object_name ? "#{object_name}[#{method}]" : method.to_s
+    id = object_name ? "#{object_name}_#{method}" : method.to_s
+    value = @object&.send(method) || ''
 
     @template.render_textarea(
-      name: "#{object_name}[#{method}]",
-      id: "#{object_name}_#{method}",
-      value: @object.send(method),
+      name: name,
+      id: id,
+      value: value,
       **options,
       &block
     )
@@ -72,11 +89,13 @@ class Shadcn::FormBuilder < ActionView::Helpers::FormBuilder
 
   def select_field(method, options = {}, &block)
     error_class = @object&.errors&.[](method)&.any? ? 'error' : ''
+    name = object_name ? "#{object_name}[#{method}]" : method.to_s
+    id = object_name ? "#{object_name}_#{method}" : method.to_s
     options[:class] = @template.tw("#{options[:class]} #{error_class}")
 
     select_html = @template.render_select(
-      name: "#{object_name}[#{method}]",
-      id: "#{object_name}_#{method}",
+      name: name,
+      id: id,
       selected: options[:selected],
       **options,
       &block
