b88179b683db038290f5cdf2bcfd85397757163f
p3rc1us <kervymaniego@gmail.com>
2024-05-31 13:34:56 +0800

    chore: added the service to pay_appointment

--- app/services/appointments/pay_appointment.rb
+++ app/services/appointments/pay_appointment.rb
@@ -7,6 +7,7 @@ module Appointments
         params[:email] = appointment.client.email
 
         Appointments::NewAppointmentMail.call(appointment.client)
+        Appointments::NewAppointmentMailToFreelancer.call(appointment)
 
         payment_method = Paymongo::PaymentMethods.create(appointment.payment_intent_id, params)
         payment_intent_id = appointment.payment_intent_id
