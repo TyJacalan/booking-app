ec8addf26ad7995cc03d217332218a3d8c0a322f
maeci <cimafrancamae@gmail.com>
2024-06-02 19:49:58 +0800

    chore: refactor services seed

--- db/seeds/services.rb
+++ db/seeds/services.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 25.times do
+  categories = Category.order('RANDOM()').limit(rand(1..3))
   Service.create!(
-    title: Faker::Job.title,
+    title: categories.first.title,
     description: Faker::Lorem.sentence(word_count: 20),
     price: Faker::Number.between(from: 100, to: 10_000),
-    categories: Category.order('RANDOM()').limit(rand(1..3)),
+    categories:,
     user_id: User.joins(:role).where(roles: { name: 'freelancer' }).sample.id
   )
 end
