9011997dfebcb274b5bbd7995ca1d32431d24f86
Ty Jac <muzuuu@protonmail.com>
2024-06-08 11:08:22 +0800

    chore: reset db in production

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exex rails db:reset
 bundle exec rails db:migrate
 bin/rails runner db/script/create_users_and_appointments.rb
