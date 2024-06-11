168ff20989f34b9dfd1d7f300b5183b575fbbdf9
Ty Jac <muzuuu@protonmail.com>
2024-06-11 13:40:14 +0800

    chore: setup rails admin gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     # omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
